7a2b50f3e8073d9a69e9e39bac0b902b08e2f9c3
Easi-d-Hatchett <easi.hatchett3@gmail.com>
2024-02-07 15:58:23 -0500

    almost complete still having erros

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const COHORT = 2401-fsa-et-web-ft-sf;
+const COHORT = "/2401-fsa-et-web-ft";
 const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
 const endpoint = "/events";
 
 
 const state = {
     events: [],
+    selectedEvent: null // Add selectedEvent property to the state object
 };
 
 const eventList = document.querySelector("#events");
 
-const addEventForm=document.querySelector("#addEvent");
-addEventForm.addEventListener("submit,addEvent");
+const addEventForm = document.querySelector("#addEvent");
+addEventForm.addEventListener("submit",addEvent);
 
 /**
  * Sync state with API and Rerender 
@@ -27,9 +28,9 @@ render();
 
 async function getEvents(){
     try {
-        const response = await  fetch(API_URL);
-        const json = await response.json();
-        state.events = json.data;
+        const response = await fetch(API_URL);
+        const parsedResponse = await response.json();
+        state.events = parsedResponse.data;
     } catch (error){
         console.error(error);
     }
@@ -39,14 +40,57 @@ async function getEvents(){
  * Render events from state
  */
 
-function renderEvents() {
-    if(!state.events.length) {
-       eventList.innerHTML=<li>"no events"</li>
-        return;
+function renderEvents() { 
+    if (state.events.length === 0) {
+    const $li =
+    document.createElement("li");
+    $li.textContent = "no events";
+    return $li;
+    } else {
+        const $events= state.events.map((event)=> {
+            const $event = document.createElement("li");
+            $event.textContent = event.name;
+
+            $event.addEventListener("click",(_event)=> {
+                setSelectedEvent (event);
+                renderSelectedEvent();
+
+            });
+
+            const $delete = document.createElement("button");
+                $delete.addEventListener("click",(_event) => deleteEvent (event.id));
+                $delete.textContent = "Delete Event";
+                $event.append($delete);
+                return $event;
+
+        });
+        eventList.replaceChildren(...$events);
     }
-}
-const eventCards =state.events.map((Event))
-=> {
-    const li = document.createElement("li");
-    li.innerHTML=<h2>${event.name}</h2>
-}
+       
+    }
+    function renderSelectedEvent() {
+        const $name = document.createElement("h2");
+        $name.textContent = state.selectedEvent.name;
+        const $time = document.createElement("li");
+        $time.textContent = state.selectedEvent.time;
+        const $date =document.createElement("li");
+        $date.textContent= state.selectedEvent.date;
+        const $location =document.createElement("li");
+        $location.textContent= state.selectedEvent.location;
+        const $description =document.createElement("li");
+        $description.textContent= state.selectedEvent.description;
+        const $eventDetails = [$name, $time, $date, $location, $description];
+
+        selectedEvent.replaceChildren(...$eventDetails);
+    }
+
+    // Script
+
+    async function render() {
+        await getEvents();
+        renderEvents();
+    renderSelectedEvent();
+    }
+
+    window.addEventListener("load", render);
+    
